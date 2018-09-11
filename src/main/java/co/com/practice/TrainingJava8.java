1f842ece334ebb1329fdbb6c47346bafc75cfdf2
Fidel Jose Villero De Hoyos <fivillero@personalsoft.com>
2018-09-11 07:54:02 -0500

    more issues solver

--- src/main/java/co/com/practice/TrainingJava8.java
+++ src/main/java/co/com/practice/TrainingJava8.java
@@ -1,18 +1,9 @@
 package co.com.practice;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TrainingJava8 {
-
-	private List<Integer> listNumbers;
-	
-	public TrainingJava8(){
-		this.listNumbers = new ArrayList<>( Arrays.asList(1,2,3,4,5,6,7));
-	}
 	
 	public List<Integer> getEvenNumbers(List<Integer> lista){
 		return lista.stream()
@@ -23,8 +14,7 @@ public class TrainingJava8 {
 	}
 	
 	public float getFreeSalary(float totalSalary) {
-		float result = totalSalary - get8Percent(totalSalary); 
-		return result;
+		return totalSalary - get8Percent(totalSalary);
 	}
 	
 	public float get8Percent(float total) {
